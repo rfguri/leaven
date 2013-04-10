2321994fa6c72ffef19f09c1f4394c7129ae3c1c
Roger Fernandez Guri <rogerfernandezguri@me.com>
2013-04-10 10:49:30 +0200

    Necessary libraries for source file

---
+++ libs.h
@@ -0,0 +1,24 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <glob.h>
+#include <netdb.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <sys/ipc.h>
+#include <sys/socket.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
