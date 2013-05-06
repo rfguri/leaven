4ddf1ce3fc5fc5cdde785e21923b5870ed0e33d9
Roger Fernandez Guri <rogerfernandezguri@me.com>
2013-05-07 01:21:54 +0200

    Source file for '/dir' operation methods

--- leaven_dir.c
+++ leaven_dir.c
@@ -1,6 +1,6 @@
 int entries_detected;
 
-void print_fat_dir_info (dir_FAT dir_fat[8]) {
+void print_fat_dir_info (dir_FAT *dir_fat) {
 
 	int i, j, ascii;
 	char aux[100];
@@ -11,8 +11,9 @@ void print_fat_dir_info (dir_FAT dir_fat[8]) {
 
 		if (dir_fat[i].size != 0xffffffff) {
 
-			if (dir_fat[i].attr & 0x02){ 
+			if (dir_fat[i].attr & 0x02){
 				// File Hidden
+
 			} else {
 
 				// Check if filename contains non ascii character
@@ -33,7 +34,7 @@ void print_fat_dir_info (dir_FAT dir_fat[8]) {
 					} else {
 						write (1, "\t", 1);
 					}
-					
+
 					if (dir_fat[i].attr & 0x20){
 						sprintf(aux, "<file>\t\t");
 					} else if (dir_fat[i].attr & 0x08) {
@@ -42,13 +43,140 @@ void print_fat_dir_info (dir_FAT dir_fat[8]) {
 						sprintf(aux, "<dir>\t\t");
 					}
 					write(1, aux, strlen(aux));
-					
+
 					sprintf (aux, "%d", dir_fat[i].size);
 					write (1, aux, strlen(aux));
 					write (1, "\n", 1);
-			
+
 				}
-			
+
+			}
+
+		}
+
+	}
+
+}
+
+void print_ext2_block_info (dir_EXT2 *dir_ext2, int i) {
+
+	char aux[100];
+
+	sprintf(aux, "%s\t", dir_ext2[i].name);
+	write (1, aux, strlen(aux));
+
+	if (dir_ext2[i].file_type & 0x01) {
+
+		sprintf(aux, "<file>\t");
+
+	} else if (dir_ext2[i].file_type & 0x02) {
+
+		sprintf(aux, "<dir>\t");
+
+	}
+
+	write(1, aux, strlen(aux));
+
+	if (dir_ext2[i].file_type & 0x01) {
+
+		// Print size
+		sprintf(aux, "\t %d\n", dir_ext2[i].file_size);
+		write (1, aux, strlen(aux));
+
+	} else {
+
+		write (1, "\n", 1);
+
+	}
+
+}
+
+void read_block (int i, int fd, dir_EXT2 *dir_ext2, int block_init_addr, int block_acc, int i_table_init_addr, filesystem_EXT2 ext2_info) {
+
+	char tmp[100];
+
+	lseek (fd, block_init_addr + block_acc, SEEK_SET);
+	read (fd, &dir_ext2[i].num_inode, 4);
+
+	read (fd, &dir_ext2[i].register_length, 2);
+
+	read (fd, &dir_ext2[i].name_length, 1);
+
+	read (fd, &dir_ext2[i].file_type, 1);
+
+	int name_lenght_aux = (int)dir_ext2[i].name_length;
+
+	int n = read (fd, tmp, name_lenght_aux);
+
+	tmp[n] = '\0';
+	dir_ext2[i].name = strdup(tmp);
+
+	if (dir_ext2[i].file_type & 0x01) {
+
+		// Go to inode
+		int i_addr = i_table_init_addr + (dir_ext2[i].num_inode - 1) * ext2_info.inode_size;
+
+		lseek (fd, 4 + i_addr, SEEK_SET);
+		read (fd, &dir_ext2[i].file_size, 4);
+
+	}
+
+}
+
+void get_ext2_dir_info (int fd, dir_EXT2 *dir_ext2, group_EXT2 *group_ext2, filesystem_EXT2 ext2_info) {
+
+	inode_EXT2 inode_ext2;
+
+	int i, addr_block = 40;
+
+	// Get inode table init block
+	lseek (fd, 8 + 2048, SEEK_SET);
+	read (fd, &(*group_ext2).init_inode_table, 4);
+
+	// Get inode table init address
+	int i_table_init_addr = ((*group_ext2).init_inode_table * ext2_info.block_size);
+
+	// Go to second inode (root)
+	int i_root_init_addr = i_table_init_addr + ext2_info.inode_size;
+
+	// Get inode size
+	lseek (fd, 4 + i_root_init_addr, SEEK_SET);
+	read (fd, &inode_ext2.i_size, 4);
+
+	// Get direct pointer number
+	int max_direct_pointers = inode_ext2.i_size / ext2_info.block_size;
+
+	int acc = 0;
+
+	dir_ext2 = (dir_EXT2 *) malloc (sizeof(dir_EXT2) * max_direct_pointers);
+
+	// Iterate trough direct inodes
+	for (i = 0; i < max_direct_pointers; i++) {
+
+		lseek (fd, (addr_block + (4 * i)) + i_root_init_addr, SEEK_SET);
+		read (fd, &inode_ext2.i_block_pointer[i], 4);
+
+		// Check last inode
+		if (inode_ext2.i_block_pointer[i] == 0) {
+
+			i = 11;
+
+		} else {
+
+			// Get block init address
+			int block_init_addr = inode_ext2.i_block_pointer[i] * ext2_info.block_size;
+
+			int block_acc = 0;
+
+			while (acc < inode_ext2.i_size && block_acc < ext2_info.block_size) {
+
+				read_block(i, fd, dir_ext2, block_init_addr, block_acc, i_table_init_addr, ext2_info);
+
+				print_ext2_block_info (dir_ext2, i);
+
+				acc = acc + dir_ext2[i].register_length;
+				block_acc = block_acc + dir_ext2[i].register_length;
+
 			}
 
 		}
@@ -109,13 +237,11 @@ void get_fat_dir_info (int fd, dir_FAT *dir_fat, filesystem_FAT fat_info, int ro
 			// Get attributes
 			lseek (fd, root_dir_init_addr + 11 + (32 * i), SEEK_SET);
 			read (fd, &dir_fat[i].attr, 1);
-			
+
 			// Get size
 			lseek (fd, root_dir_init_addr + 28 + (32 * i), SEEK_SET);
 			read (fd, &dir_fat[i].size, 4);
 
-			//printf("i: %d, filename: %s, extension: %s, attributes: %x, size: %d\n", i,dir_fat[i].filename, dir_fat[i].extension, dir_fat[i].attr, dir_fat[i].size);
-	
 		}
 
 	}
